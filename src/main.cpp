b9e75153fc66dd609a7094fcb8107dee9c5e78eb
Nikita Enin <Nikita.Enin@infowatch.com>
2022-02-15 20:32:03 +0300

    fix formating

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,16 @@
 
 namespace po = boost::program_options;
 
-bool ProessCMD(int argc, char** argv,
-                          std::filesystem::path& source_file,
-                          std::filesystem::path& destination_file,
-                          size_t& block_size)
+bool ProessCMD(int argc, char **argv,
+               std::filesystem::path &source_file,
+               std::filesystem::path &destination_file,
+               size_t &block_size)
 {
 
-    try {
+    try
+    {
         po::options_description desc("Program Usage", 1024, 512);
-        desc.add_options()
-          ("help,h", "Display help message")
-          ("source_file,sf", po::value<std::filesystem::path>(&source_file)->required(), "Source file")
-          ("destination_file,df", po::value<std::filesystem::path>(&destination_file)->required(), "Destination file")
-          ("block_size,bs", po::value<size_t>(&block_size)->default_value(job::gkDefaultBlockSize), "Hash block size")
-        ;
+        desc.add_options()("help,h", "Display help message")("source_file,sf", po::value<std::filesystem::path>(&source_file)->required(), "Source file")("destination_file,df", po::value<std::filesystem::path>(&destination_file)->required(), "Destination file")("block_size,bs", po::value<size_t>(&block_size)->default_value(job::gkDefaultBlockSize), "Hash block size");
 
         po::variables_map vm;
         po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -31,7 +27,9 @@ bool ProessCMD(int argc, char** argv,
             return false;
         }
         po::notify(vm);
-    } catch(const std::exception& ex) {
+    }
+    catch (const std::exception &ex)
+    {
         std::cerr << "Error: " << ex.what() << std::endl;
         return false;
     }
@@ -39,18 +37,19 @@ bool ProessCMD(int argc, char** argv,
     return true;
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 
     std::filesystem::path source_file, destination_file;
     size_t block_size{0};
-    if (! ProessCMD(argc, argv, source_file, destination_file, block_size)) {
+    if (!ProessCMD(argc, argv, source_file, destination_file, block_size))
+    {
         return -1;
     }
 
     job::JobController controller;
 
-    if (! controller.DoJob(std::make_unique<job::SignatureCalculatorJob<digest::DigestMD5>>(source_file, destination_file, block_size)))
+    if (!controller.DoJob(std::make_unique<job::SignatureCalculatorJob<digest::DigestMD5>>(source_file, destination_file, block_size)))
     {
         std::cout << "Failed to complete signature calculation" << std::endl;
         return -1;
